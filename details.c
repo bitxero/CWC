063aa78c0ff9a44df72a759b05049d8f3de67fdb
bitxero <bitxero@proton.me>
2025-02-21 22:59:56 +0330

    Fix stdin byte bug

--- details.c
+++ details.c
@@ -1,5 +1,5 @@
 #include "details.h"
-#define BUF_SIZE 256
+#define BUF_SIZE 1024
 
 details_t file_details(FILE *fp) {
     details_t dtls;
@@ -13,17 +13,12 @@ details_t file_details(FILE *fp) {
 }
 
 details_t stdin_details() {
+
     details_t dtls = {
         .bytes = 0,
     };
     
-    char mystr[BUF_SIZE];
-    while (fgets(mystr, BUF_SIZE, stdin)) {
-        for (int i = 0; i < BUF_SIZE; i++) {
-            if (mystr[i] == '\0') { break; }
-            dtls.bytes++;
-        }
-    }
+    while (getchar() != EOF) { dtls.bytes++; }
 
     return dtls;
 }
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "details.h"
 
 int main() {
-    FILE *fp = fopen("main.c", "r"); // temprary value
+    FILE *fp = fopen("test.txt", "r"); // temprary value
     details_t f_dtls = stdin_details();
     printf("%ld\n", f_dtls.bytes);
     return 0;
